7244e6113b03684e227a418a3785c8b475d1eb1a
unknown <rogersjohn811@gmail.com>
2021-01-17 18:25:52 -0500

    Finished workout.js

--- models/workout.js
+++ models/workout.js
@@ -7,3 +7,39 @@ const workoutSchema = new Schema({
         type: Date,
         default: () => new Date(),
     },
+    exercises: [
+        {
+            type: {
+                type: String,
+                required: "Enter an exercise type.",
+            },
+            name: {
+                type: String,
+                required: "Enter exercise name."
+            },
+            duration: {
+                type: String,
+                required: "Enter a duration"
+            },
+            weight: {
+                type: Number
+
+            },
+            reps: {
+                type: Number
+            },
+            sets: {
+                type: Number
+            },
+            distance: {
+                type: Number
+            }
+        }
+    ]
+
+  
+});
+
+const Workout = mongoose.model("Workout", workoutSchema);
+
+module.exports = Workout;
