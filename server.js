da6f242b147501fe0b342e2652fd2e129ab68d5f
unknown <rogersjohn811@gmail.com>
2021-01-17 18:13:28 -0500

    Working on workout.js

--- server.js
+++ server.js
@@ -9,3 +9,19 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 app.use(express.static("public"));
+
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/fitness-tracker", {
+  useNewUrlParser: true,
+  useFindAndModify: false,
+  useCreateIndex: true,
+  useFindAndModify: false
+});
+
+// routes
+app.use(require("./routes/api.js"));
+
+app.use(require("./routes/htmlRoutes.js"));
+
+app.listen(PORT, () => {
+  console.log(`App running on port ${PORT}!`);
+});
